f18708ad8f2ce87c9de9ccdfaa8b1bf3860fed48
liess liess <liess.inoudjal@gmail.com>
2019-11-05 21:23:32 +0100

    modification ud gitignore pour push les fichier concernant le js et css

---
+++ public/build/fontawesome.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["fontawesome"],{
+
+/***/ "./assets/css/fontawesome/scss/fontawesome.scss":
+/*!******************************************************!*\
+  !*** ./assets/css/fontawesome/scss/fontawesome.scss ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+},[["./assets/css/fontawesome/scss/fontawesome.scss","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvY3NzL2ZvbnRhd2Vzb21lL3Njc3MvZm9udGF3ZXNvbWUuc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBQSx1QyIsImZpbGUiOiJmb250YXdlc29tZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpbiJdLCJzb3VyY2VSb290IjoiIn0=
