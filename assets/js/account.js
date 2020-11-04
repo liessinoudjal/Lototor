7fabeb7fc8b66921445f297e7251e7aa6467dee2
Liess Inoudjal <liess.inoudjal@gmail.com>
2020-11-04 23:38:00 +0100

    Account : js activé que pour les organizer

--- assets/js/account.js
+++ assets/js/account.js
@@ -14,14 +14,13 @@ import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/pu
 
 Routing.setRoutingData(routes);
 
-const formAddAsso = document.querySelector("#js_form_add_association")
-const btnsubmitAddAsso = document.querySelector("#js_form_add_association button[name='newAssociationSubmit']")
-const inputSiret = document.querySelector("#js_form_add_association #siret")
+
 const modal = $("#modal_account");
 let modalTitle = document.querySelector("#exampleModalLongTitle");
 const modalBody = document.querySelector("#modal_account .modal-body");
 const loader = document.querySelector("#modal_account .modal-body").innerHTML;
 const infoList = document.querySelector("#js_info_list");
+const typeAccount= document.querySelector("#js_account_page").dataset.typeAccount
 
 //à la fermture de la modal, on remet le loader
 modal.on('hide.bs.modal', function () {
@@ -29,62 +28,6 @@ modal.on('hide.bs.modal', function () {
     modalTitle.innerHTML = ""
 });
 
-//gestion ouverture et femeture du formulaire d'ajout d'asso
-document.querySelector("#js_btn_add_association").addEventListener("click", function (e){
-    e.preventDefault();
-    $("#js_form_add_association").slideToggle();
-})
-
-
-//gestion du formulaire d'ajout d'association
-
-formAddAsso.addEventListener("submit", function(e){
-    e.preventDefault();
-    var form = new FormData(formAddAsso);
-    modal.modal("show");
-    modalTitle.innerHTML = "Association trouvée"
-    //on verifie si l'asso existe bien
-    fetch(formAddAsso.getAttribute("action"),{
-        method : 'POST',
-        headers: {
-            "X-Requested-With": "XMLHttpRequest"
-        },
-        body: form
-    }).then(function (response){
-        return response.json()
-    }).then(function (json){
-        if(json.status == 200){
-            //on ajoute le message de confirmation de l'etablissement
-            const etablissement = json.etablissement
-            modalBody.innerHTML = json.statusText+ "</br>";
-            const button = document.createElement("button");               
-            const textnode = document.createTextNode("Oui");       
-            button.appendChild(textnode); 
-            button.classList.add("btn","btn-success", "mt-2") 
-            modalBody.appendChild(button)
-            //si l'organizer    confirme l'identité de l'etablissement qu'il veut ajouter, on envoie une requette pour l'associer à son compte
-            button.addEventListener("click",function (){
-                fetch(Routing.generate("account_add_association"),{
-                    method : 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify( etablissement )
-                }).then(function(response){
-                    // console.log(reponse)
-                    window.location.reload();
-                })
-            })
-        }else{
-            modalBody.innerHTML = json.statusText;
-        }
-    }).catch(function(e){
-        console.log(e)
-    });
-    
-
-})
 //gestion affichage forumlaire edition infos personnelles
 document.querySelector("#js_edit_info").addEventListener("click",function(e){
     e.preventDefault();
@@ -146,3 +89,68 @@ document.querySelector("#js_modifier_mdp").addEventListener("click",function(e){
     })
 
 })
+
+//seulement pour les Organizer
+if("organizer" ==typeAccount){
+    const formAddAsso = document.querySelector("#js_form_add_association")
+    const btnsubmitAddAsso = document.querySelector("#js_form_add_association button[name='newAssociationSubmit']")
+    const inputSiret = document.querySelector("#js_form_add_association #siret")
+
+    //gestion ouverture et femeture du formulaire d'ajout d'asso
+document.querySelector("#js_btn_add_association").addEventListener("click", function (e){
+    e.preventDefault();
+    $("#js_form_add_association").slideToggle();
+})
+
+
+//gestion du formulaire d'ajout d'association
+formAddAsso.addEventListener("submit", function(e){
+    e.preventDefault();
+    var form = new FormData(formAddAsso);
+    modal.modal("show");
+    modalTitle.innerHTML = "Association trouvée"
+    //on verifie si l'asso existe bien
+    fetch(formAddAsso.getAttribute("action"),{
+        method : 'POST',
+        headers: {
+            "X-Requested-With": "XMLHttpRequest"
+        },
+        body: form
+    }).then(function (response){
+        return response.json()
+    }).then(function (json){
+        if(json.status == 200){
+            //on ajoute le message de confirmation de l'etablissement
+            const etablissement = json.etablissement
+            modalBody.innerHTML = json.statusText+ "</br>";
+            const button = document.createElement("button");               
+            const textnode = document.createTextNode("Oui");       
+            button.appendChild(textnode); 
+            button.classList.add("btn","btn-success", "mt-2") 
+            modalBody.appendChild(button)
+            //si l'organizer    confirme l'identité de l'etablissement qu'il veut ajouter, on envoie une requette pour l'associer à son compte
+            button.addEventListener("click",function (){
+                fetch(Routing.generate("account_add_association"),{
+                    method : 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify( etablissement )
+                }).then(function(response){
+                    // console.log(reponse)
+                    window.location.reload();
+                })
+            })
+        }else{
+            modalBody.innerHTML = json.statusText;
+        }
+    }).catch(function(e){
+        console.log(e)
+    });
+    
+
+})
+
+
+}
